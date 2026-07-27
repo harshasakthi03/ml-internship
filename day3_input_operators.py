8a6eb58587999e77935649365892c5fa8fee7c97
harshasakthi03 <harshasakthi03@gmail.com>
2026-07-27 18:01:21 +0530

    Day 3 - Input, Output and Operators

---
+++ day3_input_operators.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("Name:",name)
+print("Age:",age)
+a=20
+b=5
+print("Addition:",a+b)
+print("Subtraction",a-b)
+print("Multiplicaion:",a*b)
+print("Division:",a/b)
+print("Modulus:",a%b)
+print("Floor Division:",a//b)
+print("Power:",a**b)
