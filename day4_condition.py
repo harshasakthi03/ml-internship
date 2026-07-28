825b038603011c5abfeba876291f9054726f0bf7
harshasakthi03 <harshasakthi03@gmail.com>
2026-07-28 20:25:49 +0530

    Day 4 - Conditional Statements

---
+++ day4_condition.py
@@ -0,0 +1,31 @@
+num = int(input("Enter a number:" ))
+if num>= 0:
+    print("Positive Number")
+else:
+    print("Negative Number")
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not Eligible")
+a = int(input("Enter first number:"))
+b = int(input("Enter Second number: "))
+if a>b:
+    print("Largest:", a)
+else:
+    print("Largest:",b)
+mark = int(input("Enter your mark: "))
+if mark>= 90:
+    print("Grade A")
+elif mark >= 75:
+    print("Grade B")
+elif mark >= 50:
+    print("Grade C")
+else:
+    print("Fail")
+
+
+
+
+
+
