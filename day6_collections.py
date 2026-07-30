1d0592a35513011ef486f2cd55c6a8b010bad57b
harshasakthi03 <harshasakthi03@gmail.com>
2026-07-30 17:25:05 +0530

    Day 6 - Lists, Tuples, Sets and Dictionaries

---
+++ day6_collections.py
@@ -0,0 +1,17 @@
+fruits = ["Apple","Banana","Mango"]
+print(fruits)
+print(fruits[0])
+fruits.append("Orange")
+print(fruits)
+colors = ("Red","Green","Blue")
+print(colors)
+print(colors[1])
+numbers = {10,20,30,20,10}
+print(numbers)
+student = {
+    "name":"Harsha",
+    "age":19,
+    "course":"AIML"
+}
+print(student)
+print(student["name"])
