1172701ac31b2334b8291f7fe0de907dd9fa958b
harshasakthi03 <harshasakthi03@gmail.com>
2026-08-05 09:32:04 +0530

    Add files via upload
    Day 9 - Python Modules (Math, Random, Datetime)

---
+++ day9_modules.py
@@ -0,0 +1,23 @@
+import math
+
+print("Square Root of 25:", math.sqrt(25))
+print("Power:", math.pow(2, 3))
+print("Ceil:", math.ceil(5.2))
+print("Floor:", math.floor(5.8))
+print("Value of Pi:", math.pi)
+
+import random
+
+print("Random Number:", random.randint(1, 100))
+
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+print("Random Fruit:", random.choice(fruits))
+
+import datetime
+
+today = datetime.datetime.now()
+
+print("Current Date and Time:", today)
+print("Current Year:", today.year)
+print("Current Month:", today.month)
+print("Current Day:", today.day)
