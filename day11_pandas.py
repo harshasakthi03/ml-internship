3e84139348f9687be0ebd281678fd4e3353be5b5
harshasakthi03 <harshasakthi03@gmail.com>
2026-08-05 09:44:51 +0530

    Add files via upload
    Day 11 - Pandas Basics

---
+++ day11_pandas.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import pandas as pd
+
+marks = pd.Series([85, 90, 78, 92])
+
+print(marks)
+import pandas as pd
+
+student = {
+    "Name": ["Harsha", "Priya", "Arun"],
+    "Age": [19, 20, 19],
+    "Marks": [95, 88, 91]
+}
+
+df = pd.DataFrame(student)
+
+print(df)
