5df649ee53ad2695f49f4fc50bca88458cada348
harshasakthi03 <harshasakthi03@gmail.com>
2026-08-05 09:24:21 +0530

    Add files via upload
    Day 7 - Python Strings

---
+++ day7_strings.py
@@ -0,0 +1,26 @@
+
+name = "Harsha"
+
+print(name)
+print(name[0])
+print(name[-1])
+
+print(name[1:4])
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+sentence = "machine learning internship"
+
+print(sentence.replace("learning", "Learning"))
+print(sentence.count("n"))
+print(sentence.find("internship"))
+
+first = "Machine"
+second = "Learning"
+
+print(first + " " + second)
+
+age = 19
+print(f"My name is {name} and I am {age} years old.")
