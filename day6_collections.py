75de814003622be697ba1b5ac3eb06a31b40e079
harshasakthi03 <harshasakthi03@gmail.com>
2026-08-05 09:18:15 +0530

    Add files via upload
    Day 6 - Python Collections (List, Tuple, Set, Dictionary)

--- day6_collections.py
+++ day6_collections.py
@@ -1,17 +1,56 @@
-fruits = ["Apple","Banana","Mango"]
-print(fruits)
-print(fruits[0])
+
+fruits = ["Apple", "Banana", "Mango"]
+
+print("Original List:", fruits)
+
 fruits.append("Orange")
-print(fruits)
-colors = ("Red","Green","Blue")
-print(colors)
-print(colors[1])
+print("After Append:", fruits)
+
+fruits.remove("Banana")
+print("After Remove:", fruits)
+
+fruits.insert(1, "Grapes")
+print("After Insert:", fruits)
+
+print("First Fruit:", fruits[0])
+
+print()
+
+
+
+colors = ("Red", "Green", "Blue")
+
+print("Tuple:", colors)
+
+print("First Color:", colors[0])
+
+print()
+
+
+
+
 numbers = {10,20,30,20,10}
-print(numbers)
+
+print("Set:", numbers)
+
+numbers.add(40)
+
+print("After Add:", numbers)
+
+print()
+
+
+
 student = {
     "name":"Harsha",
     "age":19,
     "course":"AIML"
 }
+
 print(student)
+
 print(student["name"])
+
+student["city"] = "Chennai"
+
+print(student)
