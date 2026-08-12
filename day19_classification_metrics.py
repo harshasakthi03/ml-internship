cbc13ef18b641d043e9f21e2c0b846cbc94800e9
harshasakthi03 <harshasakthi03@gmail.com>
2026-08-12 09:47:28 +0530

    Add files via upload

---
+++ day19_classification_metrics.py
@@ -0,0 +1,28 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
+
+iris = load_iris()
+
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = LogisticRegression(max_iter=200)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
+print("\nConfusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
+
+print("\nPrecision:", precision_score(y_test, y_pred, average="weighted"))
+print("Recall:", recall_score(y_test, y_pred, average="weighted"))
+print("F1 Score:", f1_score(y_test, y_pred, average="weighted"))
